c61fb9531dea9447fb4efd308aff4c571330bf60
Robert Sehr <robert.sehr@intranda.com>
2023-02-24 09:24:06 +0100

    added test class and sample file

--- plugin/src/main/java/io/goobi/api/job/HerisQuartzPlugin.java
+++ plugin/src/main/java/io/goobi/api/job/HerisQuartzPlugin.java
@@ -1,13 +1,35 @@
 package io.goobi.api.job;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
+import org.apache.commons.configuration.tree.xpath.XPathExpressionEngine;
 import org.goobi.production.flow.jobs.AbstractGoobiJob;
 
+import de.sub.goobi.config.ConfigPlugins;
+import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class HerisQuartzPlugin extends AbstractGoobiJob {
+
+    private XMLConfiguration config;
+
+    @Getter
+    // TODO replace it with sftp call
+    private String herisFolder;
+
+    @Getter
+    private long lastRunMillis;
+
     @Override
     public void execute() {
+
+        parseConfiguration();
+
         // search for latest heris file
 
         // check, if it is newer than the last import
@@ -25,7 +47,25 @@ public class HerisQuartzPlugin extends AbstractGoobiJob {
 
     @Override
     public String getJobName() {
-        return "HerisJob";
+        return "intranda_quartz_herisJob";
+    }
+
+    public void parseConfiguration() {
+        config = ConfigPlugins.getPluginConfig(getJobName());
+        config.setExpressionEngine(new XPathExpressionEngine());
+        config.setReloadingStrategy(new FileChangedReloadingStrategy());
+        herisFolder = config.getString("/herisFolder");
+        lastRunMillis = config.getLong("/lastrun", 0l);
+    }
+
+    private void updateLastRun() {
+        try {
+            config.setProperty("lastRun", lastRunMillis);
+            Path configurationFile = Paths.get(config.getBasePath(), "plugin_" + getJobName() + ".xml");
+            this.config.save(configurationFile.toString());
+        } catch (ConfigurationException e) {
+            log.error("Error while updating the configuration file", e);
+        }
     }
 
 }
--- plugin/src/test/java/io/goobi/api/job/HerisQuartzPluginTest.java
+++ plugin/src/test/java/io/goobi/api/job/HerisQuartzPluginTest.java
@@ -2,17 +2,25 @@ package io.goobi.api.job;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import org.easymock.EasyMock;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.PowerMock;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
-import io.goobi.api.job.HerisQuartzPlugin;
+import de.sub.goobi.config.ConfigurationHelper;
 
-//@RunWith(PowerMockRunner.class)
-//@PowerMockIgnore({ "javax.management.*", "javax.net.ssl.*" ,"jdk.internal.reflect.*"})
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ ConfigurationHelper.class })
+@PowerMockIgnore({ "javax.management.*", "javax.net.ssl.*", "jdk.internal.reflect.*", "com.sun.org.apache.xerces.*", "javax.xml.*", "org.xml.*",
+        "org.w3c.*" })
 public class HerisQuartzPluginTest {
 
     private static String resourcesFolder;
@@ -28,12 +36,36 @@ public class HerisQuartzPluginTest {
         String log4jFile = resourcesFolder + "log4j2.xml"; // for junit tests in eclipse
 
         System.setProperty("log4j.configurationFile", log4jFile);
+
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+        PowerMock.mockStatic(ConfigurationHelper.class);
+        ConfigurationHelper configurationHelper = EasyMock.createMock(ConfigurationHelper.class);
+        EasyMock.expect(ConfigurationHelper.getInstance()).andReturn(configurationHelper).anyTimes();
+        EasyMock.expect(configurationHelper.getConfigurationFolder()).andReturn(resourcesFolder).anyTimes();
+        EasyMock.expect(configurationHelper.useS3()).andReturn(false).anyTimes();
+
+        EasyMock.replay(configurationHelper);
+        PowerMock.replay(ConfigurationHelper.class);
     }
 
     @Test
-    public void testTitle() throws IOException {
+    public void testTitle() throws Exception {
         HerisQuartzPlugin job = new HerisQuartzPlugin();
-        assertEquals("HerisJob", job.getJobName());
+        assertEquals("intranda_quartz_herisJob", job.getJobName());
+    }
+
+    @Test
+    public void testConfiguration() throws Exception {
+        HerisQuartzPlugin plugin = new HerisQuartzPlugin();
+
+        plugin.parseConfiguration();
+
+        assertEquals("/tmp/", plugin.getHerisFolder());
+        assertEquals(1, plugin.getLastRunMillis());
 
     }
 }
