0f39dbed8ad38b64f46e2a4c8291c068336b6cde
Robert Sehr <robert.sehr@intranda.com>
2023-09-05 13:31:52 +0200

    added missing import

--- plugin/src/main/java/io/goobi/api/job/HerisQuartzPlugin.java
+++ plugin/src/main/java/io/goobi/api/job/HerisQuartzPlugin.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Properties;
 
 import org.apache.commons.configuration.HierarchicalConfiguration;
 import org.apache.commons.configuration.XMLConfiguration;
